b0898885f6c9d201d75d690569cb84c4e07474a3
tram0808 <phanquynhbichtram88@gmail.com>
2020-04-13 20:36:17 +0700

    promise

--- a.js
+++ a.js
@@ -11,17 +11,18 @@
 // })
 const request = require('request');
 
-function getTempCity(cityName, cb){
-    const url =`http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityName}`
-
-    request(url, function (error, response, body) {
-        if(error) return cb(error)
-        if(JSON.parse(body).message) return cb(JSON.parse(body).message)
-        return cb(JSON.parse(body).main.temp)
+function getTempCity(cityName){
+    return new Promise((resolve, reject) => {
+        const url =`http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityName}`
+        request(url, function (error, response, body) {
+        if(error) return reject(error)
+        if(JSON.parse(body).message) return reject(JSON.parse(body).message)
+        return resolve(JSON.parse(body).main.temp)
+        });
     });
+    
 }
-getTempCity("hanoi", (error, temp) =>{
-    if(error) return console.log(error)
-    return console.log(temp)
-})
+getTempCity("phuquoc")
+.then(data => console.log(data))
+.catch(error => console.log("loi " + error))
 
