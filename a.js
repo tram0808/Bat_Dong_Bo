f49711cf86a1f53ced35471ab331dcf151ada377
tram0808 <phanquynhbichtram88@gmail.com>
2020-04-13 16:49:26 +0700

    request2

--- a.js
+++ a.js
@@ -14,16 +14,12 @@ const request = require('request');
 function getTempCity(cityName, cb){
     const url ='http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Hanoi'
     request(url, function (error, response, body) {
-        if(error){
-            return cb(error);
-        }
-        if(JSON.parse(body).main.temp){
-            return cb(JSON.parse(body).main.mesage);
-            return cb(JSON.parse(body).main.temp)
-        }
-});
+        if(error) return cb(error)
+        if((JSON.parse(body).message)) return cb(JSON.parse(body).message)
+        return cb(JSON.parse(body).main.temp)
+    });
 }
-getTempCity('hanoi', (error, temp) =>{
+getTempCity("hanoi", (error, temp) =>{
     if(error) return console.log(error)
     return console.log(temp)
 })
