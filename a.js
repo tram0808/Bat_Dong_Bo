48790a5025c39434c7629acc6ec9668ee07ca578
tram0808 <phanquynhbichtram88@gmail.com>
2020-04-13 16:46:36 +0700

    request

--- a.js
+++ a.js
@@ -1,11 +1,29 @@
-function cong(cb){
-    let a= 5
-    let b
-    setTimeout(() => {
-        b=10
-        cb(a, b)
-    }, 1000);
+// function cong(cb){
+//     let a= 5
+//     let b
+//     setTimeout(() => {
+//         b=10
+//         cb(a, b)
+//     }, 1000);
+// }
+// cong((a, b) => {
+//     console.log(a+b)
+// })
+const request = require('request');
+
+function getTempCity(cityName, cb){
+    const url ='http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Hanoi'
+    request(url, function (error, response, body) {
+        if(error){
+            return cb(error);
+        }
+        if(JSON.parse(body).main.temp){
+            return cb(JSON.parse(body).main.mesage);
+            return cb(JSON.parse(body).main.temp)
+        }
+});
 }
-cong((a, b) => {
-    console.log(a+b)
+getTempCity('hanoi', (error, temp) =>{
+    if(error) return console.log(error)
+    return console.log(temp)
 })
